f7d39b49275c342035919021e3ca287110752480
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-10 15:30:11 -0600

    Fixed a bug with expected type of props -PropTypes validator- in the 'TodoItem.jsx' component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 const TodoItem = ({ itemProp }) => <li>{itemProp.title}</li>;
 
 TodoItem.propTypes = {
-  itemProp: PropTypes.string.isRequired,
+  itemProp: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    completed: PropTypes.bool,
+  }).isRequired,
 };
 
 export default TodoItem;
